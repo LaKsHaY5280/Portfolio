17d6fcf5fecee7d38e3b2a8e14d847a1981e3223
Lakshay goyal <akshaybuster5280@gmail.com>
2023-09-18 11:25:28 +0530

    bug fix

--- components/socials.jsx
+++ components/socials.jsx
@@ -5,31 +5,26 @@ const Socials = () => {
   return (
     <div className=" layout_social ">
       <div>
-        <div>
-          <Link href="https://github.com/LaKsHaY5280" target="blank">
-            <Github color="#FB2576" absoluteStrokeWidth />
-          </Link>
-          <Link
-            href="https://www.instagram.com/akuma._.lakshay/"
-            target="blank"
-          >
-            <Instagram color="#FB2576" absoluteStrokeWidth />
-          </Link>
-          <Link
-            href="https://www.linkedin.com/in/lakshay-goyal-2a03a4270/"
-            target="blank"
-          >
-            <Linkedin color="#FB2576" absoluteStrokeWidth />
-          </Link>
-          {/* <Link href={} target="blank"> */}
-          <Twitter color="#FB2576" absoluteStrokeWidth />
-          {/* </Link> */}
-        </div>
-        <div>
-          <span className=" text-primary writing-mode: vertical-rl;">
-            akshaytester5280@gmail.com
-          </span>
-        </div>
+        <Link href="https://github.com/LaKsHaY5280" target="blank">
+          <Github color="#FB2576" absoluteStrokeWidth />
+        </Link>
+        <Link href="https://www.instagram.com/akuma._.lakshay/" target="blank">
+          <Instagram color="#FB2576" absoluteStrokeWidth />
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/lakshay-goyal-2a03a4270/"
+          target="blank"
+        >
+          <Linkedin color="#FB2576" absoluteStrokeWidth />
+        </Link>
+        {/* <Link href={} target="blank"> */}
+        <Twitter color="#FB2576" absoluteStrokeWidth />
+        {/* </Link> */}
+      </div>
+      <div>
+        <span className=" text-primary writing-mode: vertical-rl;">
+          akshaytester5280@gmail.com
+        </span>
       </div>
     </div>
   );
